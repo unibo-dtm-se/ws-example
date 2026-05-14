221cccf1d8dbc0942ae6111426e89eb346024fdf
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2026-05-14 10:56:14 +0200

    fix(release): change command to release on test-pypi

--- release.config.mjs
+++ release.config.mjs
@@ -2,12 +2,11 @@ let dryRun = (process.env.RELEASE_DRY_RUN || "false").toLowerCase() === "true";
 let testPypi = (process.env.RELEASE_TEST_PYPI || "false").toLowerCase() === "true";
 const pypiToken = process.env.PYPI_TOKEN;
 
-let prepareCmd = "poetry version -- \${nextRelease.version}" + ` && poetry config pypi-token.pypi ${pypiToken}`;
-let publishCmd = `poetry publish --build`;
+let prepareCmd = "poetry version -- \${nextRelease.version}";
+let publishCmd = `poetry publish --build --username __token__ --password ${pypiToken}`;
 
 if (testPypi) {
     publishCmd += ` --repository testpypi`;
-    prepareCmd = prepareCmd.replace("pypi-token.pypi", "pypi-token.testpypi");
 }
 
 if (dryRun) {
